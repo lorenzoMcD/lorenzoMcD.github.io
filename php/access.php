4da510b3dabeaee39ac4ba1a79a8ae9bf552792c
Lorenzo McDaniel <lsm3x@mtmail.mtsu.edu>
2019-08-14 19:41:05 -0500

    first website upload

---
+++ php/access.php
@@ -0,0 +1,31 @@
+<?php
+
+	$minAge = 21;
+	
+	if(isset($_POST['submit'])){
+		
+		if(strlen($_POST['mm'])==1)
+		$month = '0'.$_POST['mm'];
+		else 
+		$month = $_POST['mm'];
+		$agevar = $_POST['yy'].'/'.$month.'/'.$_POST['dd'];
+		
+	  $age = strtotime($agevar);
+
+	  $twentyone = strtotime("-" . $minAge . " years");
+
+	  if($age && $twentyone && $age <= $twentyone){
+
+	     header('Location: blank.html');
+
+	  }
+
+	  else{
+
+	    header('Location: www.google.com');
+
+	  }
+
+	}
+
+	?>
